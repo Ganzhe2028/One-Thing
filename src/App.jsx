2fbb754eb28fc20d49b3a555be83fe7cdf63ec86
Ganzhe <144527308+Isaac-Bao@users.noreply.github.com>
2025-04-13 22:42:29 +0800

    finish CustomForm component and fix bug which lacks {}

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { useState } from "react";
-import { ArrowRightCircleIcon } from "@heroicons/react/24/solid";
+
+// import CustomForm 这个component文件
+import CustomForm from "./components/CustomForm";
 
 function App() {
   const [thing, setThing] = useState("");
+  const [isCompleted, setIsCompleted] = useState(true);
 
   const handleSubmit = (e) => {
-    e.preventDefult();
-    console.log(e);
-  }
+    e.preventDefault();
+    setIsCompleted(false);
+  };
 
   const handleInput = (e) => {
     setThing(e.target.value);
-  }
+  };
 
   return (
     <main
@@ -21,26 +24,14 @@ function App() {
       text-slate-900 dark:text-slate-200"
     >
       <div className="grid place-items-center gap-8 m-8">
-        <h1 className="text-3xl sm:text-6xl font-bold text-center">
-          What is your "One thing"?
-        </h1>
-        <form
-          className="flex ring-4 rounded-md ring-slate-200 dark:ring-slate-800 focus-within:ring-teal-600 focus-within:ring-offset-4 bg-slate-100 ring-offset-slate-200 dark:ring-offset-slate-800"
-          onSubmit={handleSubmit}
-        >
-          <input
-            type="text"
-            className="bg-inherit rounded-md font-sans text-slate-800 py-2 px-6 focus:outline-none text-xl sm:text-2xl placeholder:text-slate-400 caret-teal-600 appearance-one w-full"
-            placeholder="Enter one thing..."
-            autoFocus
-            maxLength="64"
-            value={thing}
-            onInput={handleInput}
+        {/* So A && ()the function basically means if A then run B */}
+        {isCompleted && (
+          <CustomForm
+            thing={thing}
+            handleInput={handleInput}
+            handleSubmit={handleSubmit}
           />
-          <button className="bg-inherit rounded-md font-sans text-slate-800 py-2 px-6 focus:ouline-none focus:text-teal-600 hover:text-teal-600">
-            <ArrowRightCircleIcon className="h-12 w-12" />
-          </button>
-        </form>
+        )}
       </div>
     </main>
   );
---
+++ src/components/CustomForm.jsx
@@ -0,0 +1,31 @@
+// import library
+import { ArrowRightCircleIcon } from "@heroicons/react/24/solid";
+
+const Form = ( thing, handleInput, handleSubmit ) => {
+  return (
+    <>
+      <h1 className="text-3xl sm:text-6xl font-bold text-center">
+        What is your "One thing"?
+      </h1>
+      <form
+        className="flex ring-4 rounded-md ring-slate-200 dark:ring-slate-800 focus-within:ring-teal-600 focus-within:ring-offset-4 bg-slate-100 ring-offset-slate-200 dark:ring-offset-slate-800"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          className="bg-inherit rounded-md font-sans text-slate-800 py-2 px-6 focus:outline-none text-xl sm:text-2xl placeholder:text-slate-400 caret-teal-600 appearance-one w-full"
+          placeholder="Enter one thing..."
+          autoFocus
+          maxLength="64"
+          value={thing}
+          onInput={handleInput}
+        />
+        <button className="bg-inherit rounded-md font-sans text-slate-800 py-2 px-6 focus:ouline-none focus:text-teal-600 hover:text-teal-600">
+          <ArrowRightCircleIcon className="h-12 w-12" />
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default Form; // 允许函数被外部调用。也就是说在app.jsx里导入这份文件之后可以调用里面的函数
